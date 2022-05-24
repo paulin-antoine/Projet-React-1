30b67de6576bd5d0f082df32f03344799547d07d
Paulin Antoine <antoine_paulin@yahoo.com>
2022-05-25 00:25:52 +0200

    creating the header

--- front-end/cdm/src/App.js
+++ front-end/cdm/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Header from "./Header.js";
 
 class App extends Component {
   render() {
-    return <h1>hello world</h1>;
+    return (
+      <>
+        <Header />
+      </>
+    );
   }
 }
 
---
+++ front-end/cdm/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>Paragraphe</p>
+    </>
+  );
+}
+
+export default Header;
